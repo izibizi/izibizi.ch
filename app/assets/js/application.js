72ffb6a79d8059b618b7da3b266a9797aa01b3e0
Manuel Hutter <manuel@hutter.io>
2015-10-26 10:46:07 +0100

    more setup

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -1,16 +1,13 @@
 //= require bootstrap-sprockets
-
 $(document).ready(function() {
-  $.ajaxSetup({ cache: true });
-  $.getScript('//connect.facebook.net/de_CH/sdk.js', function () {
-    FB.init({
-      appId   : '948928015195601',
-      xfbml   : false,
-      version : 'v2.5'
-    });
-
-    FB.api('gmizibizi/albums', 'get', { fields: ['id', 'name', 'cover_photo', 'type'], limit: 999 }, function (message) {
-      console.log(message);
-    });
-  });
+  // replace images with retina-variant
+  var pixelRatio = "(-webkit-min-device-pixel-ratio: 1.5),(min--moz-device-pixel-ratio: 1.5),(-o-min-device-pixel-ratio: 3/2),(min-resolution: 1.5dppx)";
+  // check if we have a retina screen
+  if (window.matchMedia && window.matchMedia(pixelRatio).matches) {
+    var retinaImages = $('img[data-retina]');
+    for (var i = 0; i < retinaImages.length; i++) {
+      var img = retinaImages[i];
+      img.src = img.src.replace(/(\.\w+)$/, '@2$1');
+    };
+  };
 });
--- app/helpers.rb
+++ app/helpers.rb
@@ -1,9 +1,17 @@
 helpers do
+  # Menu Item
+  def menu_link(path, text)
+    active = request.path_info.eql?(path) ? ' class="active"' : ''
+    %(<li#{active}><a href="#{url(path)}">#{text}</a></li>)
+  end
+
+  # Stylesheet path
   def stylesheet(s)
     s = asset_path(s + '.css') unless s =~ /^(https?:\/)?\//
     %(<link rel="stylesheet" href="#{s}" />)
   end
 
+  # Javascript path
   def javascript(s)
     s = asset_path(s + '.js') unless s =~ /^(https?:\/)?\//
     %(<script src="#{s}"></script>)
