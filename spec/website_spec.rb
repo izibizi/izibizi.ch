fff539a55ac3ce3194934f5926bedb5e7be20fd1
Manuel Hutter <manuel@hutter.io>
2015-10-26 12:15:01 +0100

    ++~~

--- spec/website_spec.rb
+++ spec/website_spec.rb
@@ -1,9 +1,20 @@
 RSpec.describe 'Website' do
+  before { get '/' }
+
   describe '/' do
     it 'gets the homepage' do
-      get '/'
       expect(last_response).to be_ok
       expect(last_response.body).to include 'Willkommen'
+    end
+  end
+
+  describe 'Navbar' do
+    it 'has a navbar' do
+      expect(last_response.body).to include 'navbar'
+    end
+    it 'highlights the current page' do
+      exp = '<li class="active"><a href="http://example.org/">Home</a>'
+      expect(last_response.body).to include exp
     end
   end
 end
